267a0aeb60a68c8ef8ce0a49c208128196b8689a
vikv1 <vikv1@users.noreply.github.com>
2025-07-13 03:09:05 +0000

    deploy: 07ad30b4654f9b515873017c946fb4f99389f478

---
+++ sw.js
@@ -0,0 +1,73 @@
+const version = '20250713040903';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/me/academics/2025/06/15/lin_alg_1/","/me/career/reflections/2025/06/13/capital_one/","/me/general/reflections/2025/06/13/acting/","/me/categories/","/me/elements/","/me/blog/","/me/","/me/manifest.json","/me/offline/","/me/assets/search.json","/me/search/","/me/assets/styles.css","/me/thanks/","/me/redirects.json","/me/sitemap.xml","/me/robots.txt","/me/blog/page2/","/me/feed.xml","/me/assets/styles.css.map","/me/assets/logos/logo.svg", "/me/assets/default-offline-image.png", "/me/assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/me/assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
